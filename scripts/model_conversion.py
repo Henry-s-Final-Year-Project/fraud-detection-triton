89f5256cf5fba98a238305cc834beb8ef2724a81
Henry-0810 <ml2002stduy@gmail.com>
2025-04-27 22:41:24 +0100

    triton server done

---
+++ scripts/model_conversion.py
@@ -0,0 +1,21 @@
+import xgboost
+import onnxmltools
+from onnxmltools.convert import convert_xgboost
+from onnxmltools.convert.common.data_types import FloatTensorType
+
+# Load your trained .bst model
+model = xgboost.Booster()
+model.load_model("models/xgb_geolocation_fraud_model.bst")
+print(model.feature_names)
+
+feature_names = [f"f{i}" for i in range(20)]  # assuming 20 features
+model.feature_names = feature_names
+# Define input shape
+initial_type = [("input", FloatTensorType([1, 20]))]  # 20 features, batch size 1
+
+# Convert to ONNX model
+onnx_model = convert_xgboost(model, initial_types=initial_type)
+
+# Save to file
+with open("xgb_geolocation_fraud_model.onnx", "wb") as f:
+    f.write(onnx_model.SerializeToString())
